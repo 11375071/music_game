a662f3d6e9220a077f095683f9a881187db1a24d
Tastror <yccozy2001@163.com>
2023-04-01 01:38:59 +0800

    add event_deal in page

--- src/obj/band_button.py
+++
@@ -1,7 +0,0 @@
-from utils.define import PyGame
-import utils.color as color
-
-
-class BandButton:
-    def __init__(self) -> None:
-        pass
--- src/utils/page.py
+++ src/utils/page.py
@@ -1,29 +1,34 @@
-from utils.define import PyGame, StateMachine
 from typing import Any
+from pygame import event
+from utils.define import PyGame, StateMachine
+
 
 # wait for future use (refactor)
 class Page:
     """
     inherit this.
 
-    you need to overload:
-      init(), control_flow()
+    you may need to overload:
+      init(), event_deal(event), control_flow()
 
     you may need to use inside:
       add_to_xxx(), del_xxx()
-    
+
     you must use outside:
       show()
     """
-    
+
     def __init__(self,  game: PyGame, state: StateMachine) -> None:
         self.game = game
         self.state = state
         self.__inited = False
         self.__texture: list = []
         self.__bind: list = []
-    
+
     def init(self):
+        pass
+
+    def event_deal(self, event: event.Event):
         pass
 
     def control_flow(self):
@@ -32,12 +37,13 @@ class Page:
     def show(self):
         if not self.__inited:
             self.__init()
-        
+
         for event in self.game.it.event.get():
             if event.type == self.game.it.QUIT:
                 self.state.quit = True
             for i in self.__bind:
                 i.click_check(event)
+            self.event_deal(event)
 
         self.control_flow()
 
